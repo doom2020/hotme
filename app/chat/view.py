3d9db8ca1e69fd314d691b9a598b815efce1f5c1
yuanzhijian <408575225@qq.com>
2020-12-04 00:13:16 +0800

    socket处理

--- app/chat/view.py
+++ app/chat/view.py
@@ -1,5 +1,6 @@
 from app.main.view import BaseHandler
 import tornado.websocket
+from datetime import datetime
 
 
 class WebSocketBaseHandler(tornado.websocket.WebSocketHandler):
@@ -8,6 +9,7 @@ class WebSocketBaseHandler(tornado.websocket.WebSocketHandler):
 
 
 class ChatHandler(WebSocketBaseHandler):
+    user_list = list()
     def initialize(self):
         print("func: initialize")
         pass
@@ -17,15 +19,22 @@ class ChatHandler(WebSocketBaseHandler):
     #     pass
 
     def open(self):
-        if self.current_user:
+        # user_list = list()
+        if self.current_user:  # 当能获取到用户信息,将用户放入列表中
+            now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             user = self.current_user.encode("utf-8")
-        else:
-            user = '未知用户'
-        print("当前连接过来的用户: %s" % user)
-        self.write_message("欢迎: %s " % user)
+            self.user_list.append(self)
+            print("当前连接过来的用户名: %s" % user)
+            for u in self.user_list:
+                u.write_message("[%s]欢迎: %s 进入群聊" % (now_str, user))
 
     def on_close(self):
         print("func: on_close")
 
     def on_message(self, message):
-        print("收到客户都消息: %s" % message)
+        print("收到客户端消息: %s" % message)
+        if self.current_user:
+            now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            user = self.current_user.encode("utf-8")
+            for u in self.user_list:
+                u.write_message("[%s] %s: %s" % (now_str, user, message))
