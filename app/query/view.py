f0eef0ea3b1778080813377358d532a00c86503c
yuanzhijian <v-yuanzhijian@xiaomi.com>
2020-12-09 20:37:47 +0800

    httpclient 异步

--- app/query/view.py
+++ app/query/view.py
@@ -1,18 +1,55 @@
 import tornado.httpclient
+from app.main.view import BaseHandler
+import time
+from tornado import gen
 
 
-class QueryHandler(object):
+class QueryHandler(BaseHandler):
 
-    def fun1(self):
+    # async def get(self, *args, **kwargs):
+    #     """
+    #     异步请求
+    #     """
+    #     http_client = tornado.httpclient.AsyncHTTPClient()
+    #     try:
+    #         response = await http_client.fetch("http://www.douban.com")
+    #     except Exception as e:
+    #         print("error: %s" % e)
+    #     else:
+    #         print(response.body)
+    #     self.write(response.body)
+
+    # def get(self, *args, **kwargs):
+    #     """
+    #     http同步请求
+    #     """
+    #     http_client = tornado.httpclient.HTTPClient()
+    #     try:
+    #         response = http_client.fetch("http://www.douban.com")
+    #         print(response.body)
+    #     except tornado.httpclient.HTTPError as e:
+    #         print("error: %s" % e)
+    #     except Exception as e:
+    #         print("error: %s" % e)
+    #     http_client.close()
+    #     self.write(response.body)
+    # @gen.coroutine
+    # def get(self):
+    #     """
+    #     基于生成器的协程
+    #     """
+    #     http_client = tornado.httpclient.AsyncHTTPClient()
+    #     response = yield http_client.fetch("http://www.douban.com")
+    #     print(response.body)
+    #     self.write(response.body)
+
+    @gen.coroutine
+    def get(self):
         """
-        http同步请求
+        生成器协程多任务并发
         """
-        http_client = tornado.httpclient.HTTPClient()
-        try:
-            response = http_client.fetch("www.baidu.com")
-            print(response.body)
-        except tornado.httpclient.HTTPError as e:
-            print("error: %s" % e)
-        except Exception as e:
-            print("error: %s" % e)
-        http_client.close()
+        http_client = tornado.httpclient.AsyncHTTPClient()
+        response1, response2 = yield [http_client.fetch('http://www.baidu.com'), http_client.fetch('http://www.google.com')]
+        print(response1)
+        print(response2)
+    
